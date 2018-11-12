821f62e0e6492c8be8cbf98ecdb610a5f3db3aa5
cedeo <cedeo@10.50.138.130>
2018-11-12 15:01:13 +0100

    Added Methods to DataAccessor (Close #10)

--- Delfin/src/delfin/data/DataAccessor.java
+++ Delfin/src/delfin/data/DataAccessor.java
@@ -1,5 +1,12 @@
 package delfin.data;
 
+import delfin.logic.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author vikke
@@ -11,4 +18,58 @@ public class DataAccessor {
     public DataAccessor(DBConnector connector) {
         this.connector = connector;
     }
-}
+    
+    public List<Member> getMembers() throws IllegalArgumentException {
+        try{
+            String query = "SELECT * FROM member;"; 
+
+            Connection connection = connector.getConnection();  
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<Member> members = new ArrayList();
+
+            while (rs.next()) {      
+                members.add(new Member(rs.getString("ssn"), rs.getString("name"), rs.getString("adress"), rs.getString("phone"), new ActivityInfo(StatusEnum.ACTIVE, TeamEnum.SENIOR, ActivityEnum.COMPETITIOR)));
+                
+            }
+            return members;       
+        }catch (Exception ex) {     
+            ex.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+    }
+     
+      public Member getSsn(String ssn) throws IllegalArgumentException {
+        try{
+            String query = "SELECT * FROM member WHERE ssn = '" + ssn + "';";
+
+            Connection connection = connector.getConnection();  
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            
+            while (rs.next()) {      
+                return new Member (rs.getString("ssn"), rs.getString("name"), rs.getString("adress"), rs.getString("phone"), new ActivityInfo(StatusEnum.ACTIVE, TeamEnum.SENIOR, ActivityEnum.COMPETITIOR));
+                
+            }
+            throw new IllegalArgumentException();       
+        }catch (Exception ex) {     
+            ex.printStackTrace();
+            throw new IllegalArgumentException();
+        } 
+    }
+    
+      public Member createMember(String ssn, String name, String adress, String phone, ActivityInfo info) throws IllegalArgumentException {
+        try{
+            String query = "INSERT INTO members (ssn, name, adress, phone, info)";
+          
+            Connection connection = connector.getConnection();  
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+          
+        }catch (Exception ex){
+            ex.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+
