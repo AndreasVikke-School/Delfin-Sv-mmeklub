fc00d6ba087422e11f1d66c19fff931ba5d298fe
cedeo <cedeo@10.50.138.130>
2018-11-13 09:08:28 +0100

    Created coach class and test - Also, changed cities to capital letter (Close #6)

--- Delfin/test/delfin/logic/CoachTest.java
+++ Delfin/test/delfin/logic/CoachTest.java
@@ -14,8 +14,27 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author cedeo
+ * @author Celina
  */
 public class CoachTest {
 
+    @Test
+    public void createCoachTest(){
+        ActivityInfo info = new ActivityInfo(StatusEnum.ACTIVE, TeamEnum.JUNIOR, ActivityEnum.COMPETITIOR);
+        Coach c = new Coach("1705981598", "Anna Kofoed", "Dildhaven 9, 2730 Herlev", "17725889", info);
+        assertNotNull(c);
+        assertNotNull(c.getSsn());
+        assertNotNull(c.getName());
+        assertNotNull(c.getAddress());
+        assertNotNull(c.getPhone());
+        assertNotNull(c.getInfo());
+        assertNotNull(c.getAge());
+        assertSame(c.getSsn(), c.getId());
+        assertEquals(c.getSsn().length(), 10);
+        assertEquals(c.getPhone().length(), 8);
+        assertEquals(c.getAge(), 20);
+        assertEquals(c.getName(), "Anna Kofoed");
+        assertNotEquals(c.getAddress(), "Bølgehaven 5, 2730 Ravnsborg");
+        
+    }
 }
