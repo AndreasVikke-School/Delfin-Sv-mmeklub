af47a3fdb535c8b68f09064d2fd3d40d9c0fa75c
Krigermus <zyjinxx@hotmail.com>
2018-11-13 11:35:59 +0100

    changed showMember gui

--- Delfin/src/delfin/presentation/ShowMember.java
+++ Delfin/src/delfin/presentation/ShowMember.java
@@ -6,10 +6,8 @@
 package delfin.presentation;
 import delfin.data.*;
 import delfin.logic.Member;
-
-
-
-
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 /**
  *
  * @author Joe
@@ -21,6 +19,7 @@ public class ShowMember extends javax.swing.JFrame {
      */
     public ShowMember() {
         initComponents();
+        
         int count = 0;
         DataAccessor data = null;
         try{
@@ -33,6 +32,8 @@ public class ShowMember extends javax.swing.JFrame {
         }
         int rowCount = 0;
         if( jTable1.getRowCount() < count){
+            DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+            model.addRow(new Object[]{});
         for(Member m: data.getMembers()) {
             jTable1.setValueAt(m.getSsn(), rowCount, 0);
             jTable1.setValueAt(m.getSsn(), rowCount, 1);
@@ -40,8 +41,18 @@ public class ShowMember extends javax.swing.JFrame {
             jTable1.setValueAt(m.getAddress(), rowCount, 3);
             jTable1.setValueAt(m.getPhone(), rowCount, 4);
             rowCount ++;
-        } 
+            
+            }     
         }
+        TableColumn column = null;
+        for (int i = 0; i < 5; i++) {
+        column = jTable1.getColumnModel().getColumn(i);
+        if (i == 2) {
+            column.setPreferredWidth(100); //third column is bigger
+        } else {
+            column.setPreferredWidth(50);
+        }
+}
     }
 
     /**
@@ -60,13 +71,20 @@ public class ShowMember extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
                 "Id", "Ssn", "Name", "Address", "Phone", "Activity", "Contender", "Category"
             }
         ));
+        jTable1.setEnabled(false);
+        jTable1.setRowSelectionAllowed(false);
         jTable1.setShowGrid(true);
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -88,6 +106,14 @@ public class ShowMember extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        
+        /*boolean flag = jTable1.isEditing();
+        if (flag == false) {
+            JOptionPane.showMessageDialog(null, "You can not edit cells!", "Warning", HEIGHT);
+        }*/
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -119,11 +145,15 @@ public class ShowMember extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ShowMember().setVisible(true);
+                ShowMember sM = new ShowMember();
+                sM.setTitle("Members");
+                sM.setVisible(true);
+                
+                
             }
         });
     }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
