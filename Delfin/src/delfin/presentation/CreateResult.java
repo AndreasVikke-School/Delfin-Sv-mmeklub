b62e853de12c149c5edc16abb407a9558e00e0c6
cedeo <cedeo@10.50.138.130>
2018-11-14 17:19:42 +0100

    Update Delfin/src/delfin/presentation/CreateResult.java

--- Delfin/src/delfin/presentation/CreateResult.java
+++ Delfin/src/delfin/presentation/CreateResult.java
@@ -204,11 +204,14 @@ public class CreateResult extends javax.swing.JFrame {
     }//GEN-LAST:event_timeTextFieldActionPerformed
                                               
     private void createButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createButtonActionPerformed
-    try {
+     try {
         if(ssnTextField.getText().length() != 10 || Pattern.matches("[a-zA-Z]+", ssnTextField.getText())) {
             showErrorMessage("Please type a valid SSN with maximum ten digits)");
         }
-        else if(dayTextField.getText().length() != 2 || Pattern.matches("[a-zA-Z]+", ssnTextField.getText())) {
+        
+        
+        
+        else if(dayTextField.getText().length() != 2 || Pattern.matches("[a-zA-Z]+", dayTextField.getText())) {
             showErrorMessage("Please type a valid day with two digits");
         }
         else if(Integer.parseInt(dayTextField.getText()) > 31 && 0 > Integer.parseInt(dayTextField.getText())) {
@@ -216,27 +219,46 @@ public class CreateResult extends javax.swing.JFrame {
         }
         
         
-        else if(monthTextField.getText().length() != 2 || Pattern.matches("[a-zA-Z]+", ssnTextField.getText())) {
+        
+        else if(monthTextField.getText().length() != 2 || Pattern.matches("[a-zA-Z]+", monthTextField.getText())) {
             showErrorMessage("Please type a valid month with two digits");
         }
-        else if(yearTextField.getText().length() != 4) {
-            showErrorMessage("Please type a valid year with four digits");
-        }
-        else if(timeTextField.getText().length() != 4) {
-            showErrorMessage("Please type a valid time with four digits");
+        else if(Integer.parseInt(monthTextField.getText()) > 12 && 0 > Integer.parseInt(monthTextField.getText())) {
+            showErrorMessage("Please type a valid month between 12 and 1");
         }
         
-        else{
+        
+       
+        else if(yearTextField.getText().length() != 4 || Pattern.matches("[a-zA-Z]+", ssnTextField.getText())) {
+            showErrorMessage("Please type a valid year with four digits");
+        }
+        //////////////////////////////////////////////////////////////////////////// time placement event
+        else if(Integer.parseInt(yearTextField.getText()) > 2018 && 0000 > Integer.parseInt(yearTextField.getText())) {
+            showErrorMessage("Please type a valid year");
+        }
+        
+        else if(timeTextField.getText().length() != 5 || Pattern.matches("[a-zA-Z]+", timeTextField.getText())) {
+            showErrorMessage("Please type a valid time with four digits");
+        }
+        else if(Double.parseDouble(timeTextField.getText()) > 60.00 && 00.00 > Double.parseDouble(timeTextField.getText())) {
+            showErrorMessage("Please type a valid time");
+        }       
+        
+        else if(placementTextField.getText().length() != 1 || Pattern.matches("[a-zA-Z]+", placementTextField.getText())) {
+            showErrorMessage("Please type a valid placement");
+        }
+
+        else {
         Result result = new Result(ssnTextField.getText(), LocalDate.of(new Integer(Integer.parseInt(dayTextField.getText())), Integer.parseInt(monthTextField.getText()), Integer.parseInt(yearTextField.getText())), Double.parseDouble(timeTextField.getText()), Integer.parseInt(placementTextField.getText()), eventTextField.getText());
           
         da.create(result);
-        }  
-    }catch(Exception ex) {
+        } 
+    } catch(Exception ex) {
         ex.printStackTrace();
-        showErrorMessage(ex.getMessage());
+        showErrorMessage(ex.getMessage());        
+    }  
     }//GEN-LAST:event_createButtonActionPerformed
-    }
-    
+   
     private void placementTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_placementTextFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_placementTextFieldActionPerformed
