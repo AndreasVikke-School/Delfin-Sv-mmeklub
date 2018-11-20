de3e95aa3644ca5b9930f916044f814c41882554
Krigermus <zyjinxx@hotmail.com>
2018-11-20 12:11:06 +0100

    FærdigMedAtLege

--- Delfin/src/delfin/Delfin.java
+++ Delfin/src/delfin/Delfin.java
@@ -1,11 +1,6 @@
 package delfin;
 
-import delfin.data.DataAccessorMemberFile;
-import delfin.logic.ActivityEnum;
-import delfin.logic.ActivityInfo;
-import delfin.logic.Member;
-import delfin.logic.StatusEnum;
-import delfin.logic.TeamEnum;
+import delfin.presentation.Menu;
 /**
  *
  * @author Andreas Vikke, Nina Lisakovski, Martin Frederiksen & Celina Dencker
@@ -13,15 +8,10 @@ import delfin.logic.TeamEnum;
 public class Delfin {
 
     public static void main(String[] args) {
-        /*java.awt.EventQueue.invokeLater(new Runnable() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Menu().setVisible(true);
             }
-        });*/
-        DataAccessorMemberFile dAMF = new DataAccessorMemberFile();
-        System.out.println(dAMF.getSingleById("2011020223"));
-        
-        Member m = new Member("0101021322", "Test", "Test", "23135434", new ActivityInfo(StatusEnum.ACTIVE, TeamEnum.SENIOR, ActivityEnum.EXERCISER));
-        dAMF.create(m);
+        });
     }
 }
