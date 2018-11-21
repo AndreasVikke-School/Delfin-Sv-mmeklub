866ca06c20a21a6d7e00d869c005ff45ddc15b65
Nina <Nina@10.50.130.82>
2018-11-21 11:52:32 +0100

    Event GUI created

--- Delfin/src/delfin/logic/controller/EventController.java
+++ Delfin/src/delfin/logic/controller/EventController.java
@@ -9,7 +9,6 @@ import delfin.data.DBConnector;
 import delfin.data.DataAccessorEvent;
 import delfin.logic.Event;
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -34,6 +33,10 @@ public class EventController {
         return (List<Event>)(Object)dae.getAll();
     }
     
+    public Event getSingleEvent(String id) {
+        return (Event)dae.getSingleById(id);
+    }
+    
     public String createEvent(String name, String day, String month, String year){
         try{
             if(name.length() == 0){
@@ -55,8 +58,38 @@ public class EventController {
                 return ("Please type a valid year with four digits");
             }
             else{
-                Event event = new Event(0, name, LocalDate.of(Integer.parseInt(day), Integer.parseInt(month), Integer.parseInt(year)));
+                Event event = new Event(0, name, LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day)));
                 dae.create(event);
+                return ("<html><font color='green'>Event is created successfully!</font></html>");
+            }
+        }catch(Exception ex) {
+            return (ex.getMessage());
+        }
+    }
+    
+     public String updateEvent(String name, String day, String month, String year){
+        try{
+            if(name.length() == 0){
+                return ("Please input a name");
+            }  
+            else if(day.length() != 2 || Pattern.matches("[a-zA-Z]+", day)) {
+                return ("Please type a valid day with two digits");
+            }
+            else if(Integer.parseInt(day) > 31 && 0 > Integer.parseInt(day)){
+                return ("Please type a valid day between 1 and 31");
+            }
+            else if(month.length() != 2 || Pattern.matches("[a-zA-Z]+", month)) {
+                return ("Please type a valid month with two digits");
+            }
+            else if (Integer.parseInt(month) > 12 && 0 > Integer.parseInt(month)){
+                return ("Please type a valid month between 1 and 12");
+            }
+            else if (year.length() != 4 || Pattern.matches("[a-zA-Z]+",year)){
+                return ("Please type a valid year with four digits");
+            }
+            else{
+                Event event = new Event(0, name, LocalDate.of(Integer.parseInt(day), Integer.parseInt(month), Integer.parseInt(year)));
+                dae.update(event);
                 return ("<html><font color='green'>Event is created successfully!</font></html>");
             }
         }catch(Exception ex) {
